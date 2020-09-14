b100f5324ac3b21710bedee93d76f471d419da96
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-14 11:45:21 +0000

    products rendering on products URL - finally - object vs objects error

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def allproducts(request):
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)   # view to return products page including sorts&search queries
