8783f4058a284d0c0808aba2f74782e643ee4111
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-29 17:56:36 +0000

    stripe card payments initial link working

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HWm7zIK2eXI7ATXGsk3QPPOcxeiWh0cxdEtDsRGjrjRm4tAuCdUJzMxgnhe55Mgnlf4spNTtYNfNwu4fhyarHXs00eVnXcqwE',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
