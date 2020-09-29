959663a8e4bd95818a72919af2af36feb4cc4ce0
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-29 18:23:03 +0000

    save update - new version of previous update to reflect style changes

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -2,20 +2,5 @@ var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
-var style = {
-    base: {
-        color: '#fc6251',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
-    },
-    invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
-    }
-};
-var card = elements.create('card', {style: style});
+var card = elements.create('card',);
 card.mount('#card-element');
