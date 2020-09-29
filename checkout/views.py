3cf8966b8580da32afe05475d63def79e69d31b9
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-29 19:20:46 +0000

    Stripe Payment Test Working to Strip Dashboard

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cartcontents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your Cart is Empty")
         return redirect(reverse('products'))
 
+    currentcart = cartcontents(request)
+    total = currentcart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HWm7zIK2eXI7ATXGsk3QPPOcxeiWh0cxdEtDsRGjrjRm4tAuCdUJzMxgnhe55Mgnlf4spNTtYNfNwu4fhyarHXs00eVnXcqwE',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
