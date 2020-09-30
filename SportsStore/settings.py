163cc62e62bd479c89974e9708b795113a445e17
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-30 17:55:26 +0000

    removed secret key and set debug

--- SportsStore/settings.py
+++ SportsStore/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ip(gyw7re)b-wrz07tx!(x&$qa((1*q767-g$^flvz_yn!3e$j'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['aobsportsstore.herokuapp.com', 'localhost']
 
