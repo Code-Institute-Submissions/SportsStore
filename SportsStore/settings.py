3bc2771ae4b5b2689d2432ef9b75b5555e5e1ff4
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-30 17:45:03 +0000

    Trying to deploy

--- SportsStore/settings.py
+++ SportsStore/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'SportsStore.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES - {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
