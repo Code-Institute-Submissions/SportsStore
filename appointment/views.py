c71384320ad0f9922a4a70a4f3368856fd6e43c6
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-30 16:41:19 +0000

    pre-initial deploy

--- appointment/views.py
+++ appointment/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 # Create your views here.
 
 def viewappointment(request):
     return render(request, 'appointment/appointment.html')   # view to return appointment
+
