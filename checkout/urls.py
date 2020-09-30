0e8ae734feebafa8557fbdba6fb2444612b9fb3e
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-30 11:38:15 +0000

    added class methods to webhook hander and webhook view

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkoutsuccess/<order_number>', views.checkoutsuccess, name='checkoutsuccess'),
+    path('wh/', webhook, name='webhook'),
 ]
