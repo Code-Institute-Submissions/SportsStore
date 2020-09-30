fc068dbdad371e9abd78fbff7fb507b8784c03d5
Murphj99 <john.murphy99@mail.dcu.ie>
2020-09-30 17:01:56 +0000

    connected to postgres

--- SportsStore/settings.py
+++ SportsStore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +125,9 @@ DATABASES = {
 }
 
 
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -176,15 +180,15 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
 
-if 'DEVELOPMENT' in os.environ:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL= 'aobsportsstore@example.com'
+#if 'DEVELOPMENT' in os.environ:
+#    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#    DEFAULT_FROM_EMAIL= 'aobsportsstore@example.com'
 
-else: 
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT  = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.eniviron.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.eniviron.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.eniviron.get('EMAIL_HOST_USER')
+#else: 
+#    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#    EMAIL_USE_TLS = True
+#    EMAIL_PORT  = 587
+#    EMAIL_HOST = 'smtp.gmail.com'
+#    EMAIL_HOST_USER = os.env.get('EMAIL_HOST_USER')
+#    EMAIL_HOST_PASSWORD = os.env.get('EMAIL_HOST_PASS')
+#    DEFAULT_FROM_EMAIL = os.env.get('EMAIL_HOST_USER')
